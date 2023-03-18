f207f300e5205eff44d4e94158f08ec3ab45dfca
arsentii27 <pachomov93@mail.ru>
2023-03-18 23:27:05 +0100

    first commit

---
+++ eunews/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class EunewsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'eunews'
+    verbose_name = 'Новости мира'
